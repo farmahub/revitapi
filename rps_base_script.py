cbc1d579ad2b24472c4709c0b20be8929648038a
Farhang Malek <farmamailbox@gmail.com>
2025-08-07 13:20:07 +0330

    primal definition
    The most basic script template to get revit api accessed. Through this script, following items are availale:
    
    1. Any element which are manually selected in the scene could be retrieved by ElementId
    2. Any element could be retrieved through category or class
    3. Transactions could be commited or rolled-back in case of exceptions
    
    Notice: Basic python knowledge is required.

---
+++ rps_base_script.py
@@ -0,0 +1,19 @@
+from Autodesk.Revit.DB import Transaction, FilteredElementCollector
+
+
+uiapp = __revit__
+uidoc = uiapp.ActiveUIDocument
+doc = uidoc.Document
+
+selection = uidoc.Selection.GetElementIds()
+collector = FilteredElementCollector(doc)  # Must get defined at every attempt
+
+t = Transaction(doc, "TRANSACTION_NAME")
+
+try:
+	t.Start()
+	# Transactions here ...
+	t.Commit()
+except Exception as e:
+	print("Exception:", e)
+	t.RollBack()
